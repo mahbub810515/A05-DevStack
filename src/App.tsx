19a790760c77c6490df3a26cfe362b9bd58def7f
mahbub810515 <mahbub810515@gmail.com>
2026-09-13 14:48:45 +0600

    initial setup done

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>welcome to devstack app</h1>
+      <h5>welcome to devstack app</h5>
+    </>
+  )
+}
+
+export default App
