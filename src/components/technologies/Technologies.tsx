9ce3f946fa14edeef41815bee0e9d62a59726232
mahbub810515 <mahbub810515@gmail.com>
2026-09-13 16:46:12 +0600

    show technoloies item by using technologyCard component

--- src/components/technologies/Technologies.tsx
+++ src/components/technologies/Technologies.tsx
@@ -1,5 +1,6 @@
 import { use } from "react"
 import type { TechnologyType } from "../../types/TechnologyType"
+import TechnologyCard from "./TechnologyCard"
 
 export type TechnologiesProps = {
     technologiesPromise: Promise<TechnologyType[]>
@@ -15,6 +16,14 @@ const Technologies = ({ technologiesPromise }: TechnologiesProps) => {
                 <h1 className='font-extrabold text-6xl pb-6'>Explore the<span className=' bg-linear-to-r from-orange-500 to-purple-500 bg-clip-text text-transparent'>Technologies</span></h1>
                 <p className='font-normal text-18px text-primary-black mb-2'>Pick one technology per category to build your ideal stack. </p>
             </div>
+             <div className="grid grid-cols-[3fr_1fr]">
+                <div className="grid grid-cols-3 gap-4 border-primary-black border">
+                    {technologies.map(technology => <TechnologyCard
+                        technology={technology}                       
+                    />)}
+                </div>
+                </div>
+            
         </div>
     )
 }
---
+++ src/components/technologies/TechnologyCard.tsx
@@ -0,0 +1,31 @@
+import { FaStar } from "react-icons/fa"
+import type { TechnologyType } from "../../types/TechnologyType"
+
+
+export type TechnologyProps ={
+    technology: TechnologyType   
+
+}
+const TechnologyCard = ({ technology }: TechnologyProps) => {
+    return (
+        <div className="border-primary-black rounded border p-5">
+            <div className="flex justify-between mb-3">
+                <img className="w-10 h-10" src={technology.icon} alt="icon" />
+                <button className="bg-slate-200 rounded-xl px-2">{technology.badge}</button>
+            </div>
+            <h2 className="font-bold text-lg mb-3">{technology.name}</h2>
+            <p className="mb-4">{technology.description}</p>
+            <div className="card-bottom">
+                <div className="flex justify-between mb-4">
+                    <p className="bg-slate-200 rounded-xl px-2">{technology.category}</p>
+                    <p>{technology.difficulty}</p>
+                    <p className="flex items-center gap-1"><FaStar className="text-amber-300" />{technology.rating}</p>
+                </div>
+                <button className="w-full font-medium text-3 text-white rounded bg-slate-500 py-2 px-20">Add to Stack</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default TechnologyCard
