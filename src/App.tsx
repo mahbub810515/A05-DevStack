ae84c5f3303011ee97b4fd0cb5d1544445f67513
mahbub810515 <mahbub810515@gmail.com>
2026-09-13 16:19:36 +0600

    fetch the data and pass to technologies component by using suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
+import { Suspense } from 'react';
 import './App.css'
 import Banner from './components/Banner'
 import Nav from './components/Nav'
+import Technologies from './components/technologies/Technologies';
+
+const technologyDataFetch=async()=>{
+  const res =await fetch('/data.json');
+  const data = await res.json();
+  return data;
+} 
 
 function App() {
- 
+  const technologiesPromise= technologyDataFetch();
 
   return (
     <>
       <Nav/>
       <Banner/>
+      <Suspense fallback={<p>Loading....</p>}>
+        <Technologies technologiesPromise={technologiesPromise} /> 
+      </Suspense>
     </>
   )
 }
---
+++ src/components/technologies/Technologies.tsx
@@ -0,0 +1,12 @@
+
+
+
+const Technologies = ({technologiesPromise}) => {
+  return (
+    <div>
+        <h1>Technologies</h1>
+    </div>
+  )
+}
+
+export default Technologies
