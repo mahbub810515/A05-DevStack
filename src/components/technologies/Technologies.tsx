4a8e31852982ac7adcbd54c618a36d2f6bce7ce1
mahbub810515 <mahbub810515@gmail.com>
2026-09-13 20:46:00 +0600

    fix explore technologies border issue and build remove all button logic

--- src/components/technologies/Technologies.tsx
+++ src/components/technologies/Technologies.tsx
@@ -29,20 +29,21 @@ const Technologies = ({ technologiesPromise }: TechnologiesProps) => {
                 <h1 className='font-extrabold text-6xl pb-6'>Explore the<span className=' bg-linear-to-r from-orange-500 to-purple-500 bg-clip-text text-transparent'>Technologies</span></h1>
                 <p className='font-normal text-18px text-primary-black mb-2'>Pick one technology per category to build your ideal stack. </p>
             </div>
-            <div className="grid grid-cols-[3fr_1fr]">
-                <div className="grid grid-cols-3 gap-4 border-primary-black border">
+            <div className="grid grid-cols-[3fr_1fr] gap-2">
+                <div className="grid grid-cols-3 gap-4 border-primary-black">
                     {technologies.map(technology => <TechnologyCard
                         technology={technology}
                         handleTechnologyStack={handleTechnologyStack}
                     />)}
                 </div>
-                <div className="rounded-2xl p-2">
+                <div className="self-start border rounded-2xl p-2">
                     <h2 className="font-bold text-2xl">Your Stack</h2>
                     <h3 className="mb-2">{technologyStack.length} Technology Selected</h3>
                     {technologyStack.map(stackItem=><YourStackCard 
                     stackItem={stackItem}
                     handleTechnologyStack={handleTechnologyStack}
                     />)}
+                <button onClick={()=>setTechnologyStack([])} className="w-full font-bold text-[14px] text-red-400 border border-red-400 rounded-[10px] mt-4 py-2">Remove All</button>
                 </div>
             </div>
 
